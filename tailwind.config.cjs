e72d9327e5e4397b0aa3aca04d01020d46257ef1
Nicolas.S <nicolas.webdev@proton.me>
2023-05-31 16:03:22 +0200

    Hero done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      primary: "#FFBA19",
+      primaryShade: "#ffd675",
+      primaryDark: "#805d0d",
+      bodyTextColor: "#4E4B66",
+      bodyTextColorWhite: "#FAFBFC",
+      headerColor: "#1a1a1a",
+      black: "#000000",
+    },
+    fontFamily: {
+      roboto: "Roboto, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
